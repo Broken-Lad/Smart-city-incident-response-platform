a7ec33bab104cb59991b458b505f92fa986427da
Bilal <119631217+Futurestt@users.noreply.github.com>
2025-12-02 11:44:39 +0100

    Backend

---
+++ incidentendashboard/src/main/kotlin/nl/hu/inno/incidentendashboard/IncidentendashboardApplication.kt
@@ -0,0 +1,11 @@
+package nl.hu.inno.incidentendashboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IncidentendashboardApplication
+
+fun main(args: Array<String>) {
+	runApplication<IncidentendashboardApplication>(*args)
+}
---
+++ incidentendashboard/src/test/kotlin/nl/hu/inno/incidentendashboard/IncidentendashboardApplicationTests.kt
@@ -0,0 +1,13 @@
+package nl.hu.inno.incidentendashboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IncidentendashboardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
