ddfe2221efa67b68315af53cffb45aa5c0fef95d
Patrick <patrick.damkat@student.hu.nl>
2025-12-02 12:03:21 +0100

    add some boilerplate code

---
+++ src/main/kotlin/nl/hu/inno/incidentendashboard/application/DashboardService.kt
@@ -0,0 +1,7 @@
+package nl.hu.inno.incidentendashboard.application
+
+import org.springframework.stereotype.Service
+
+@Service
+class DashboardService {
+}
---
+++ src/main/kotlin/nl/hu/inno/incidentendashboard/presentation/DashboardController.kt
@@ -0,0 +1,9 @@
+package nl.hu.inno.incidentendashboard.presentation
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/dashboard")
+class DashboardController {
+}
