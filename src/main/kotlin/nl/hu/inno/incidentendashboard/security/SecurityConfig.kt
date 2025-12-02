d5ff6f7e7c55c68bbae532766db541d76dcad0b0
Patrick <patrick.damkat@student.hu.nl>
2025-12-02 14:55:37 +0100

    open cors and make sure all endpoints are

--- src/main/kotlin/nl/hu/inno/incidentendashboard/security/SecurityConfig.kt
+++ src/main/kotlin/nl/hu/inno/incidentendashboard/security/SecurityConfig.kt
@@ -2,6 +2,8 @@ package nl.hu.inno.incidentendashboard.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -17,5 +19,15 @@ class SecurityConfig {
                     .allowedHeaders("*")
             }
         }
+    }
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .authorizeHttpRequests { auth ->
+                auth.anyRequest().permitAll()
+            }
+        return http.build()
     }
 }
