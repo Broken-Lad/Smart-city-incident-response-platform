9abf980165f66aa84d43ae79564a3fcae34d23a0
Patrick <patrick.damkat@student.hu.nl>
2025-12-02 11:18:52 +0100

    make separate files for pages

---
+++ smart-city/src/pages/about.js
@@ -0,0 +1,8 @@
+export function renderAbout() {
+  return `
+    <div>
+      <h1>About</h1>
+      <p>Dit platform is ontwikkeld als prototype voor de Smart City Incident Response hackathon. Het doel is om incidenten in een slimme stad te detecteren, analyseren en oplossen met inzet van verschillende disciplines.</p>
+    </div>
+  `;
+}
